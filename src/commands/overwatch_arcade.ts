184275bfbe6fe947d70fdb8a97bf7141898cca45
Martín (Netux) Rodríguez <send.email@netux.site>
2022-09-11 17:07:52 -0300

    feat: Implement /overwatch-arcade command
    Fetches and displays info from https://overwatcharcade.today/

---
+++ src/commands/overwatch_arcade.ts
@@ -0,0 +1,107 @@
+import { time, TimestampStyles } from "@discordjs/builders";
+import { ChatInputCommand, Command } from "@sapphire/framework";
+import axios from "axios";
+import { parseISO } from "date-fns";
+import { MessageEmbed } from "discord.js";
+import winston from "winston";
+
+import OllieBotError from "../lib/OllieBotError";
+
+import type { ClientRequest } from "http";
+import type { AxiosError } from "axios";
+import type { owtAPIResponse, owtOverwatchTodayData } from "..";
+
+
+export default class OverwatchArcade extends Command {
+  public constructor(context : Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: "overwatch-arcade",
+      description: "Get information about arcade gamemodes currently available"});
+  }
+
+  public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+    registry.registerChatInputCommand(
+      (builder) => builder
+        .setName(this.name)
+        .setDescription(this.description),
+      {
+        // TODO(netux): populate with this command's ID
+        // idHints: [],
+        guildIds: process.env.DISCORD_GUILD_ID != null ? [process.env.DISCORD_GUILD_ID] : []
+      });
+  }
+
+  public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    const response = await callOverwatchArcadeTodayAPI<owtOverwatchTodayData>("/overwatch/today");
+    console.debug({ response }); // TODO(netux): remove debug log
+
+    const createdAt = parseISO(response.createdAt);
+
+    const embed = new MessageEmbed()
+      .setTitle("Overwatch Arcade")
+      .setURL("https://overwatcharcade.today/overwatch")
+      .setColor("#a2dc26")
+      .setDescription(`As of ${response.isToday ? "today" : time(createdAt, TimestampStyles.ShortDate)} (${time(createdAt, TimestampStyles.RelativeTime)})`)
+      .addFields(
+        [
+          {
+            name: "Modes",
+            value: response.modes
+              .map((mode) => {
+                let item = `- **${mode.name}** ${mode.players}`;
+                if (mode.label) {
+                  item += ` (${mode.label})`;
+                }
+                return item;
+              })
+              .join("\n")
+          }
+        ]
+      )
+      .setFooter({
+        text: "Powered by overwatcharcade.today",
+        iconURL: "https://raw.githubusercontent.com/OverwatchArcade/Frontend/main/static/img/ow-arcade-115.png"
+      });
+
+    await interaction.reply({
+      embeds: [embed]
+    });
+  }
+}
+
+async function callOverwatchArcadeTodayAPI<D>(path: string, params?: Record<string, string | boolean | null>): Promise<D> {
+  const apiURL = new URL("https://api.overwatcharcade.today/api/v1");
+  apiURL.pathname += `${path.startsWith("/") ? "" : "/"}${path}`;
+  if (params != undefined) {
+    for (const key in params) {
+      const value = params[key];
+      if (value != null) {
+        apiURL.searchParams.set(key, encodeURIComponent(value.toString()));
+      }
+    }
+  }
+
+  const response = await axios.get<owtAPIResponse>(apiURL.toString())
+    .catch((error: AxiosError) => {
+      let message : string;
+      let errorCode : string;
+      if (error.response) {
+        // Non-200 status
+        message = `overwatcharcade.today responded with code ${response.status} - ${response.data.errorMessages?.join(", ") ?? JSON.stringify(response.data)}\nRequest: \`${(error.request as ClientRequest).path}\``;
+        errorCode = "Beaver";
+      }
+      else if (error.request) {
+        message = `Failed to contact overwatcharcade.today: ${error.request} did not get a response`;
+        errorCode = "Tortoise";
+      }
+      else {
+        message = `Axios initialization error: Malformed request? ${error.message}`;
+        errorCode = "Foxhound";
+      }
+      winston.debug(error.config);
+      throw new OllieBotError(message, errorCode);
+    });
+
+  return response.data.data as D;
+}
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
+// #region workshop.codes
 export interface wscPost {
   code: string,
   nice_url?: string,
@@ -30,3 +31,31 @@ export interface wscWikiArticle {
   created_at: string,
   updated_at: string
 }
+// #endregion
+
+// #region overwatcharcade.today
+export interface owtAPIResponse<D = unknown> {
+  data: D,
+  success: boolean,
+  errorMessages: string[],
+  statusCode: number
+}
+
+export interface owtMode {
+  name: string,
+  players: string,
+  description: string,
+  /** URL */
+  image: string,
+  /** Usually "Daily" or null */
+  label: string
+}
+
+export interface owtOverwatchTodayData {
+  isToday: boolean,
+  modes: owtMode[],
+  /**e.g. 2022-09-11T00:00:18.8106243 */
+  createdAt: string,
+  contributor: unknown // TODO: fill later if needed
+}
+// #endregion
